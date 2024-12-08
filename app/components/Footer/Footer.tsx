639f64a2addb0a3a140f96b4c3d715cb26cc36a0
DiogoCarcau <160623299+DiogoCarcau@users.noreply.github.com>
2024-12-08 15:13:04 +0000

    header e footer

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return <footer className={styles.footer}
+  >2024 22302390 Diogo Contins</footer>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default function page() {
-  return (
-    <div>vai te foder bernas</div>
-  )
+  return <>
+  <h1>React e Next.js</h1>
+  <p>Seja Bem-Vindo a minha aplicação em React e Next.js,
+    estas são algumas das tecnologias mais utilizadas
+    e vou dar um cheirinho de como brilhar nestas tecnologias!
+  </p>
+  </>
+    
 }
